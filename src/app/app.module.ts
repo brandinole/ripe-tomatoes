f81cdb87d33153ec8bfc2cd82f9dec33b797b24e
brandinole <70599440+brandinole@users.noreply.github.com>
2023-07-10 14:38:24 -0500

    designed login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LottieModule } from 'ngx-lottie';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieComponent } from './movie/movie.component';
+import { FormsModule } from '@angular/forms';
+
+export function playerFactory(): any {  
+  return import('lottie-web');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LottieModule.forRoot({ player: playerFactory }), 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  username = "";
+  password = "";
+  errorMsg = "";
+
+  constructor(private auth: AuthService, private router: Router) {
+  
+  }
+
+  options: AnimationOptions = {
+    path: '/assets/lottie/1879-movie-loading.json'
+  };
+
+  onAnimate(animationItem: AnimationItem): void {    
+    console.log(animationItem);  
+  }
+
+  login() {
+    if (this.username.trim().length === 0) {
+      this.errorMsg = "Username is required";
+    } else if (this.password.trim().length === 0) {
+      this.errorMsg = "Password is required";
+    } else {
+      this.errorMsg = ""
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home'])
+      }
+      if (res === 403) {
+        this.errorMsg = "Username or Password is incorrect"
+      }
+    }
+  }
+}
